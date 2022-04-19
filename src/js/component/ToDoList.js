5ee9346c975e978e571d34fab0ff1c67258acd52
jsowers13 <jsowers13@gmail.com>
2022-04-19 22:29:18 +0000

    completed fetch request code

--- src/js/component/ToDoList.js
+++ src/js/component/ToDoList.js
@@ -43,7 +43,7 @@ const ToDoList = () => {
 
 		fetch(apiURL, {
 			method: "PUT",
-			body: JSON.stringify(data),
+			body: JSON.stringify(newArray),
 			headers: {
 				"Content-Type": "application/json",
 			},
@@ -62,41 +62,6 @@ const ToDoList = () => {
 				//error handling
 				console.log(error);
 			});
-	};
-
-	const newEntry = () => {
-		fetch(apiURL, {
-			method: "PUT",
-			body: JSON.stringify(data),
-			headers: {
-				"Content-Type": "application/json",
-			},
-		})
-			.then((resp) => {
-				console.log(resp.ok); // will be true if the response is successfull
-				console.log(resp.status); // the status code = 200 or code = 400 etc.
-				console.log(resp.text()); // will try return the exact result as string
-				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-			})
-			.then((data) => {
-				//here is were your code should start after the fetch finishes
-				console.log(data); //this will print on the console the exact object received from the server
-			})
-			.catch((error) => {
-				//error handling
-				console.log(error);
-			});
-
-		// fetch(apiURL, {
-		// 	method: "PUT",
-		// 	body: JSON.stringify([...data]),
-		// 	headers: {
-		// 		"Content-Type": "application/json",
-		// 	},
-		// })
-		// 	.then((response) => response.json())
-		// 	.then((response) => JSON.stringify(response));
-		// .then((newTask) => setNewTask(newTask));
 	};
 
 	const newToDo = () => {
@@ -113,19 +78,32 @@ const ToDoList = () => {
 	};
 
 	const deleteItem = (index) => {
-		fetch("https://jsonplaceholder.typicode.com/posts" + index, {
-			method: "DELETE",
-		})
-			.then((response) => response.json())
-			.then((json) => {
-				setdata(json);
-				console.log(json);
-			});
-		// console.log(index);
-		// let newArr = [...data];
-		// newArr.splice(index, 1);
+		console.log(index);
+		let newArr = [...data];
+		newArr.splice(index, 1);
 
-		// setdata(newArr);
+		setdata(newArr);
+		fetch(apiURL, {
+			method: "PUT",
+			body: JSON.stringify(newArr),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			//.then((data) => {
+			//here is were your code should start after the fetch finishes
+			//console.log(data); //this will print on the console the exact object received from the server
+			//})
+			.catch((error) => {
+				//error handling
+				console.log(error);
+			});
 	};
 
 	const handleInput = (event) => {
