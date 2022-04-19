6e862643965e24c19ddcc2d00a446fa178662e77
jsowers13 <jsowers13@gmail.com>
2022-04-19 20:10:13 +0000

    changed fetch code

--- src/js/component/ToDoList.js
+++ src/js/component/ToDoList.js
@@ -14,6 +14,7 @@ const ToDoList = () => {
 		{ label: "Do the replits", done: false },
 	]);
 	const apiURL = "https://assets.breatheco.de/apis/fake/todos/user/jonsowers";
+
 	const [isShown, setIsShown] = useState({
 		state: false,
 		index: 0,
@@ -21,11 +22,11 @@ const ToDoList = () => {
 
 	useEffect(() => {
 		getInfo();
-		newEntry();
+		// newEntry();
 	}, []);
 
 	const getInfo = () => {
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/jonsowers")
+		fetch(apiURL)
 			.then((response) => response.json())
 			.then((json) => {
 				setdata(json);
@@ -39,18 +40,62 @@ const ToDoList = () => {
 
 		console.log(newArray);
 		setdata(newArray);
-	};
 
-	const newEntry = () => {
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/jonsowers", {
+		fetch(apiURL, {
 			method: "PUT",
-			body: JSON.stringify([...data]),
+			body: JSON.stringify(data),
 			headers: {
 				"Content-Type": "application/json",
 			},
 		})
-			.then((response) => response.json())
-			.then((response) => JSON.stringify(response));
+			.then((resp) => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then((data) => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch((error) => {
+				//error handling
+				console.log(error);
+			});
+	};
+
+	const newEntry = () => {
+		fetch(apiURL, {
+			method: "PUT",
+			body: JSON.stringify(data),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then((data) => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch((error) => {
+				//error handling
+				console.log(error);
+			});
+
+		// fetch(apiURL, {
+		// 	method: "PUT",
+		// 	body: JSON.stringify([...data]),
+		// 	headers: {
+		// 		"Content-Type": "application/json",
+		// 	},
+		// })
+		// 	.then((response) => response.json())
+		// 	.then((response) => JSON.stringify(response));
 		// .then((newTask) => setNewTask(newTask));
 	};
 
